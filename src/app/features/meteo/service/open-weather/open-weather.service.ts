716c2b67106255d694dbce06610ba8a7cca10a0a
webitou <goo@webitou.net>
2019-08-13 09:16:43 +0200

    modif https

--- src/app/features/meteo/service/open-weather/open-weather.service.ts
+++ src/app/features/meteo/service/open-weather/open-weather.service.ts
@@ -18,6 +18,6 @@ export class OpenWeatherService {
     // return this._http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this._appId}&units=metric`);
     // return this._http.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=57f418f213db4f946aedb7245126b2d8`);
     // return this._http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${this._appId}`);
-    return this._http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=57f418f213db4f946aedb7245126b2d8&units=metric`);
+    return this._http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=57f418f213db4f946aedb7245126b2d8&units=metric`);
   }
 }
